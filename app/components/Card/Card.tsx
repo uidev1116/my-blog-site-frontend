462f9d7948fa1380d3b57eca3cf3aa25a16baa06
uidev1116 <uidev1116@gmail.com>
2023-08-12 17:46:18 +0900

    feat: implement blog datetime and tag

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,19 +1,47 @@
-import Image from 'next/image';
+import { formatISO9075, format } from 'date-fns';
 
 type Props = {
   title: string;
   description?: string;
+  datetime: Date;
+  tags?: { name: string; url: string }[];
 };
 
-export default function Card({ title = '', description = '' }: Props) {
+export default function Card({
+  title = '',
+  description = '',
+  datetime,
+  tags = [],
+}: Props) {
   return (
-    <div className="block rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    <div className="flex flex-col gap-y-2 rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <div className="flex items-center justify-between">
+        <time
+          dateTime={formatISO9075(datetime)}
+          className="text-sm font-light text-gray-600 dark:text-gray-400"
+        >
+          {format(datetime, 'yyyy年MM月dd日')}
+        </time>
+      </div>
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {title}
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
         {description}
       </p>
+      {tags.length > 0 && (
+        <div>
+          <ul className="inline-flex flex-wrap gap-x-2">
+            {tags.map((tag) => (
+              <li key={tag.name}>
+                <span className="rounded bg-yellow-100 px-2.5 py-0.5 text-xs font-medium text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300">
+                  {tag.name}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }

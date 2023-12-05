223d8037d2077ac4ecc67e91be60be4e5122417c
uidev1116 <uidev1116@gmail.com>
2023-12-06 00:28:15 +0900

    fix: Zennの記事一覧で最後のページ時に前へのリンクが表示されない問題の修正

--- app/blog/zenn/page/[page]/page.tsx
+++ app/blog/zenn/page/[page]/page.tsx
@@ -62,31 +62,31 @@ export default async function ZennArticlesPage({
                     <div>
                       <ZennArticleList articles={articles} />
                     </div>
-                    {nextPage != null && (
-                      <div className="flex justify-end">
-                        <Link
-                          href={`/blog/zenn/${
-                            prevPage ? `page/${prevPage}` : ''
-                          }`}
-                          className="me-3 flex h-10 items-center justify-center rounded-lg border border-gray-300 bg-white px-4 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    <div className="flex justify-end">
+                      <Link
+                        href={`/blog/zenn/${
+                          prevPage ? `page/${prevPage}` : ''
+                        }`}
+                        className="me-3 flex h-10 items-center justify-center rounded-lg border border-gray-300 bg-white px-4 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                      >
+                        <svg
+                          className="me-2 h-3.5 w-3.5 rtl:rotate-180"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
                         >
-                          <svg
-                            className="me-2 h-3.5 w-3.5 rtl:rotate-180"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 14 10"
-                          >
-                            <path
-                              stroke="currentColor"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M13 5H1m0 0 4 4M1 5l4-4"
-                            />
-                          </svg>
-                          前へ
-                        </Link>
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M13 5H1m0 0 4 4M1 5l4-4"
+                          />
+                        </svg>
+                        前へ
+                      </Link>
+                      {nextPage != null && (
                         <Link
                           href={`/blog/zenn/page/${nextPage}`}
                           className="flex h-10 items-center justify-center rounded-lg border border-gray-300 bg-white px-4 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
@@ -108,8 +108,8 @@ export default async function ZennArticlesPage({
                             />
                           </svg>
                         </Link>
-                      </div>
-                    )}
+                      )}
+                    </div>
                   </div>
                 ) : (
                   <EmptyState />

1860f5131cd356e4869984f902ccea15b419626c
uidev1116 <uidev1116@gmail.com>
2023-12-10 18:19:22 +0900

    style: プロフィールページのスタイルをブログページに合わせる

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -26,13 +26,17 @@ export default async function ProfilePage() {
     <Container>
       <main className="bg-white dark:bg-gray-900">
         <div className="mx-auto flex max-w-screen-xl justify-between px-4">
-          <article className="format format-sm format-blue mx-auto w-full max-w-2xl dark:format-invert sm:format-base lg:format-lg">
-            <header className="not-format mb-4 lg:mb-6">
-              <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 dark:text-white lg:mb-6 lg:text-4xl">
+          <article className="mx-auto w-full max-w-2xl space-y-4 break-words lg:space-y-6">
+            <header className="space-y-4 lg:space-y-6">
+              <h1 className="text-3xl font-extrabold leading-tight text-gray-900 dark:text-white lg:text-4xl">
                 {entry.title}
               </h1>
             </header>
-            {entry.units !== undefined && <UnitIndex units={entry.units} />}
+            {entry.units !== undefined && (
+              <div className="format format-sm dark:format-invert sm:format-base lg:format-lg">
+                <UnitIndex units={entry.units} />
+              </div>
+            )}
           </article>
         </div>
       </main>

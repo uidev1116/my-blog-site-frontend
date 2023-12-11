72b5d8fe6d17798fd56e101ad10e97a46484cf23
uidev1116 <uidev1116@gmail.com>
2023-12-11 20:28:23 +0900

    fix: 本番環境で画像が表示されない問題の修正

--- app/config/acms.ts
+++ app/config/acms.ts
@@ -1,6 +1,6 @@
 export const API_HOST = `https://${process.env.ACMS_API_HOST}`;
 export const API_KEY = process.env.ACMS_API_KEY;
 export const ASSETS_HOST = `https://${process.env.ACMS_ASSETS_HOST}`;
-export const MEDIA_BASE_URL = `https://${process.env.ACMS_MEDIA_BASE_URL}`;
-export const ARCHIVES_BASE_URL = `https://${process.env.ACMS_ARCHIVES_BASE_URL}`;
-export const STORAGE_BASE_URL = `https://${process.env.ACMS_STORAGE_BASE_URL}`;
+export const MEDIA_BASE_URL = `${ASSETS_HOST}/media/`;
+export const ARCHIVES_BASE_URL = `${ASSETS_HOST}/archives/`;
+export const STORAGE_BASE_URL = `${ASSETS_HOST}/storage/`;

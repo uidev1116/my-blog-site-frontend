5467c6c60d7b1aa7cdf245e9588b617a949a1d4a
uidev1116 <uidev1116@gmail.com>
2023-12-15 22:34:37 +0900

    fix: IOS Safariのズーム対策でコンボボックスのフォントサイズを16pxに修正

--- app/components/BlogSearchForm/BlogSearchForm.tsx
+++ app/components/BlogSearchForm/BlogSearchForm.tsx
@@ -91,7 +91,7 @@ export default function BlogSearchForm({ id }: Props) {
         <input
           type="text"
           name="keyword"
-          className="block w-full rounded-lg border border-gray-300 bg-gray-50 py-2 pl-10 pr-8 text-sm text-gray-900 focus:border-primary focus:ring-primary dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary dark:focus:ring-primary"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 py-2 pl-10 pr-8 text-base text-gray-900 focus:border-primary focus:ring-primary dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary dark:focus:ring-primary"
           placeholder="検索..."
           {...inputProps}
         />
@@ -107,7 +107,7 @@ export default function BlogSearchForm({ id }: Props) {
   function renderMenu(
     ...args: Parameters<ComponentProps<typeof ComboBox>['renderMenu']>
   ) {
-    const [isOpen, items, inputValue, getMenuProps, menuItems] = args;
+    const [isOpen, , , getMenuProps, menuItems] = args;
     return (
       <div>
         <ul

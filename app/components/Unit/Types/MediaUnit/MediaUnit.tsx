23064ef822dfa16319dc62231535e79fbf105102
uidev1116 <uidev1116@gmail.com>
2023-12-19 00:55:55 +0900

    fix: メディアのリンクURLが空の場合にエラーが発生する問題の修正

--- app/components/Unit/Types/MediaUnit/MediaUnit.tsx
+++ app/components/Unit/Types/MediaUnit/MediaUnit.tsx
@@ -7,21 +7,23 @@ import { ASSETS_HOST, MEDIA_BASE_URL } from '@/app/config/acms';
 export default function MediaUnit({ media }: Unit<MediaUnit>) {
   const render = () => {
     if (media.type === 'image' || media.type === 'svg') {
-      const { pathname } = new URL(media.link);
       return (
         <>
           <ConditionalWrapper
             condition={media.link !== ''}
-            wrapper={(children) => (
-              <a
-                href={new URL(pathname, ASSETS_HOST).toString()}
-                className="js-smartphoto"
-                data-group={media.eid}
-                data-caption={media.caption.replace(/\r?\n/g, '')}
-              >
-                {children}
-              </a>
-            )}
+            wrapper={(children) => {
+              const { pathname } = new URL(media.link);
+              return (
+                <a
+                  href={new URL(pathname, ASSETS_HOST).toString()}
+                  className="js-smartphoto"
+                  data-group={media.eid}
+                  data-caption={media.caption.replace(/\r?\n/g, '')}
+                >
+                  {children}
+                </a>
+              );
+            }}
           >
             <Image
               src={new URL(media.path, MEDIA_BASE_URL).toString()}

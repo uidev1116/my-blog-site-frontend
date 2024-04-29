b461bc1fa7f0001f0f06e0dcb1a42efb077fd065
uidev1116 <uidev1116@gmail.com>
2024-04-30 03:21:26 +0900

    feat: implement rss feed

---
+++ app/feed/api/index.ts
@@ -0,0 +1,39 @@
+import { BASE_URL } from '@/app/config';
+import acmsClient from '@/app/lib/acms';
+import { resolveRequestCache } from '@/app/utils';
+import { AcmsContext } from '@uidev1116/acms-js-sdk/acmsPath';
+
+export type RssFeedItem = {
+  title: string;
+  link: string;
+  pubDate: Date;
+  category: string;
+  permalink: string;
+  creator: string;
+};
+
+export type RssFeed = {
+  items: RssFeedItem[];
+  lastBuildDate: Date;
+};
+
+export async function getRssFeed(
+  acmsContext: AcmsContext = {},
+): Promise<RssFeed> {
+  const { data } = await acmsClient.get(
+    { ...acmsContext, api: 'feed' },
+    { requestInit: { cache: resolveRequestCache() } },
+  );
+  const { 'item:loop': items = [], lastBuildDate } = data;
+  return {
+    items: items.map((item: any) => ({
+      title: item.title,
+      link: new URL(new URL(item.link).pathname, BASE_URL).toString(),
+      pubDate: new Date(item.pubDate),
+      category: item.category,
+      permalink: new URL(new URL(item.permalink).pathname, BASE_URL).toString(),
+      creator: item.creator,
+    })),
+    lastBuildDate: new Date(lastBuildDate),
+  };
+}

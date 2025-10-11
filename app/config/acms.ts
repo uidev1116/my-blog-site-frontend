b4add863bb4a56fe22fcf54fc5aa9f14c298ec01
uidev1116 <uidev1116@gmail.com>
2025-10-12 03:00:03 +0900

    .env の ACMS_API_HOSとACMS_ASSETS_HOSTにプロトコルを含めるように修正

--- app/config/acms.ts
+++ app/config/acms.ts
@@ -1,7 +1,7 @@
-export const API_HOST = `https://${process.env.ACMS_API_HOST}`;
+export const API_HOST = process.env.ACMS_API_HOST;
 export const API_KEY = process.env.ACMS_API_KEY;
 export const PREVIEW_KEY = process.env.ACMS_API_PREVIEW_KEY;
-export const ASSETS_HOST = `https://${process.env.ACMS_ASSETS_HOST}`;
+export const ASSETS_HOST = process.env.ACMS_ASSETS_HOST;
 export const MEDIA_BASE_URL = `${ASSETS_HOST}/media/`;
 export const ARCHIVES_BASE_URL = `${ASSETS_HOST}/archives/`;
 export const STORAGE_BASE_URL = `${ASSETS_HOST}/storage/`;
